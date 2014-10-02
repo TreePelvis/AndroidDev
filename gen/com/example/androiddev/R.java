a889a82633caae252b8c3e9fa4712dd6fd7b5a19
TreePelvis <scott-shea@hotmail.com>
2014-10-01 20:11:58 -0400

    Display a loading screen
    A loading screen is displayed while the application runs thru a
    intensive computation. This is done on a seperate thread. When its
    finished, the main layout is displayed.

--- gen/com/example/androiddev/R.java
+++ gen/com/example/androiddev/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f060016;
         public static final int action_menu_presenter=0x7f060017;
         public static final int action_mode_close_button=0x7f060024;
-        public static final int action_settings=0x7f06003e;
+        public static final int action_settings=0x7f06003f;
         public static final int activity_chooser_view_content=0x7f060025;
         public static final int always=0x7f06000b;
         public static final int beginning=0x7f060011;
@@ -1112,6 +1112,7 @@ containing a value of this type.
         public static final int image=0x7f060027;
         public static final int listMode=0x7f060001;
         public static final int list_item=0x7f060029;
+        public static final int loading=0x7f06003c;
         public static final int middle=0x7f060012;
         public static final int never=0x7f060009;
         public static final int none=0x7f060010;
@@ -1139,7 +1140,7 @@ containing a value of this type.
         public static final int text=0x7f06003d;
         public static final int title=0x7f06002b;
         public static final int top_action_bar=0x7f060020;
-        public static final int trigger=0x7f06003c;
+        public static final int trigger=0x7f06003e;
         public static final int up=0x7f060021;
         public static final int useLogo=0x7f060003;
         public static final int withText=0x7f06000c;
@@ -1194,8 +1195,9 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int loading=0x7f030018;
+        public static final int main=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0d0000;
@@ -1249,8 +1251,8 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0b000b;
         public static final int action_settings=0x7f0b000d;
         public static final int app_name=0x7f0b000e;
-        public static final int text=0x7f0b0010;
-        public static final int trigger=0x7f0b000f;
+        public static final int hello=0x7f0b0010;
+        public static final int loading=0x7f0b000f;
     }
     public static final class style {
         /** 
--- src/com/example/androiddev/MainActivity.java
+++ src/com/example/androiddev/MainActivity.java
@@ -1,50 +1,44 @@
 package com.example.androiddev;
 
 import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.os.Handler;
+import android.os.Message;
 
+public class MainActivity extends ActionBarActivity implements Runnable {
 
-public class MainActivity extends ActionBarActivity {
-	 //keep track of button presses, a main thread task
-    private int buttonPress=0;
-    TextView mButtonLabel;
-
-    //counter of time since app started, a background task
-    private TextView mTimeLabel;
-    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main); 
-       
-        mTimeLabel = (TextView) findViewById(R.id.text);
-        mButtonLabel = (TextView) findViewById(R.id.trigger);
-        /*new Timer().schedule(new TimerTask() {
-            public void run() {
-                mTimeLabel.setText("seconds remaining: " 
-                        + sec++);                
+        setContentView(R.layout.loading);        
+
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+
+    @SuppressLint("HandlerLeak") private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            setContentView(R.layout.main);
+        }
+    };
+
+    public void run(){
+        initializeArrays();
+        mHandler.sendEmptyMessage(0);
+    }
+
+    final static int NUM_SAMPS = 1000;
+    static double[][] correlation;
+    void initializeArrays() {
+        if(correlation!=null) return;
+
+        correlation = new double[NUM_SAMPS][NUM_SAMPS];
+        //calculation
+        for(int k=0; k<NUM_SAMPS; k++) {
+            for(int m=0; m<NUM_SAMPS; m++) {
+                correlation[k][m] = Math.cos(2*Math.PI*(k+m)/1000);
             }
-        }, 0, 1000);
-        */
-        new CountDownTimer(30000, 1000) {
-            public void onTick(long millisUntilFinished) {
-                mTimeLabel.setText("seconds remaining: " 
-                        + millisUntilFinished / 1000);
-                }     
-            public void onFinish() {
-                mTimeLabel.setText("done!");
-                }  
-       }.start();
-       
-        Button startButton = (Button) findViewById(R.id.trigger);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view){
-                mButtonLabel.setText("Pressed " + ++buttonPress + " times");
-            }
-        });        
+        }
     }
 }
